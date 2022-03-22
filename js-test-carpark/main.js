c0aa1d7ef2704dce69afd46250f977fe514bd156
kentdt <kent.dang1706@gmail.com>
2022-03-23 02:35:27 +0700

    fix code

--- js-test-carpark/main.js
+++ js-test-carpark/main.js
@@ -6,21 +6,28 @@ if (carStatus == 'in') {
      var curDate = new Date().getTime();
      curDate = new Date(curDate);
      localStorage.setItem('deliveryDate', curDate);
-     // Set up a series of prompts, default responses, validation functions and action functions
      var dialogData = [
           {
                id: 1,
                str: "Nhập bảng số xe: ",
-               r: "",//default response
+               r: "", //default response
                validateFn: function (r) {
                     var pattern = /[0-9]{2}-[A-Za-z][0-9]-[0-9]{4,5}/;
-                    //console.log(pattern.test(r));
-               if (! (r.length <= 11 && pattern.test(r))) {
+                    if (! (r.length <= 11 && pattern.test(r))) {
                     alert("Vui lòng nhập đúng dữ liệu bảng số xe! XX-XX-XXXXX");
-               }
+                    return false;
+                    }
+                    oldList = JSON.parse(localStorage.getItem('listData'));
+                    if (oldList !== null) {
+                         const findCode = oldList.find(({ licensePlate }) => licensePlate === r);
+                         if (findCode !== undefined) {
+                              alert('Xe đã có trong bãi vui lòng nhập lại!');
+                              return false;
+                         }
+                    }
                     return r.length <= 11 && pattern.test(r);
                }, //returns true if successful, otherwise false
-               actionFn: function(r) { //what to do if successful
+               actionFn: function(r) { //if successful
                     licensePlate = r;
                     localStorage.setItem('licensePlate', r);
                }
@@ -28,27 +35,27 @@ if (carStatus == 'in') {
           {
                id: 2,
                str: "Nhập loại xe: ",
-               r: "",//default response
+               r: "", //default response
                validateFn: function (r) {
                     if (!(r !== '' && isNaN(Number(r)))) {
                          alert("Vui lòng nhập loại xe dạng text!");
                     }
                     return r !== '' && isNaN(Number(r));
-               },//returns true if successful, otherwise false
-               actionFn: function(r) {//what to do if successful
+               }, //returns true if successful, otherwise false
+               actionFn: function(r) {//if successful
                     carType = r;
                     localStorage.setItem('carType', r);
                }
           },
      ];
 
-     var go = true;//flag that determines whether to continue looping through dialogData.
+     var go = true;
      for (var i=0; go && i < dialogData.length; i++) {
           go = promptDialog(dialogData[i], carStatus);
      }
-     document.write("Bảng số xe: " + window.localStorage.getItem('licensePlateIn') + "<br />");
-     document.write("Loại xe: " + window.localStorage.getItem('carType') + "<br />");
-     document.write("Ngày giờ gửi: " + window.localStorage.getItem('deliveryDate') + "<br />");
+     document.write("Bảng số xe: " + localStorage.getItem('licensePlate') + "<br />");
+     document.write("Loại xe: " + localStorage.getItem('carType') + "<br />");
+     document.write("Ngày giờ gửi: " + localStorage.getItem('deliveryDate') + "<br />");
 
      listDataInput =
           [
@@ -67,12 +74,6 @@ if (carStatus == 'in') {
           localStorage.setItem('listData', JSON.stringify(newlistData));
           localStorage.setItem('deliveryDate', curDate);
      }
-     const fs = require('browserify-fs')
-     let data = "Hello and Welcome to linuxhint.com"
-     fs.writeFile('file.txt', data, (err) => {
-     // error handling using throw
-     if (err) throw err;
-     })
 
 }
 //xe ra
@@ -81,7 +82,7 @@ if (carStatus == 'out') {
           [
                {
                     str: "Nhập bảng số xe: ",
-                    r: "",//default response
+                    r: "", //default response
                     validateFn: function (r) {
                     var pattern = /[0-9]{2}-[A-Za-z][0-9]-[0-9]{4,5}/;
                     oldList = JSON.parse(localStorage.getItem('listData'));
@@ -92,29 +93,36 @@ if (carStatus == 'out') {
                     }
                     return r.length <= 11 && pattern.test(r);
                     }, //returns true if successful, otherwise false
-                    actionFn: function(r) { //what to do if successful
+                    actionFn: function(r) { //if successful
                          //tinh phi va remove xe
                          var curDate = new Date().getTime();
                          curDate = new Date(curDate);
                          localStorage.setItem('outDate', curDate);
-                         carCost = carCost(r, oldList);
-                         localStorage.setItem('carCost', carCost);
+                         oldList = JSON.parse(localStorage.getItem('listData'));
+                         cost = carCost(oldList);
+                         localStorage.setItem('carCost', cost);
                          const newList = oldList.filter(({licensePlate}) => licensePlate !== r);
                          localStorage.setItem('listData', JSON.stringify(newList));
                     }
                }
           ];
-     var go = true;//flag that determines whether to continue looping through dialogData.
+     var go = true;
      for (var i=0; go && i < dialogData.length; i++) {
           go = promptDialog(dialogData[i], carStatus);
      }
+     document.write("Bảng số xe: " + localStorage.getItem('licensePlate') + "<br />");
+     document.write("Loại xe: " + localStorage.getItem('carType') + "<br />");
+     document.write("Ngày giờ ra: " + localStorage.getItem('outDate') + "<br />");
+     document.write("Giá tiền: " + localStorage.getItem('carCost') + " vnd <br />");
 }
+
 
 function carCost(listData)
 {
      var fee = 5000;
      const deliveryDate = new Date(listData[0].ngaygui);
-     const outDate = new Date(localStorage.getItem('outDate'));
+     const outDate = new Date('Fri Mar 25 2022 02:23:54 GMT+0700 (Indochina Time)');
+     //const outDate = new Date(localStorage.getItem('outDate'));
      const diffInMs = outDate - deliveryDate;
      const diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
      if (diffInDays >= 1) {
@@ -124,29 +132,23 @@ function carCost(listData)
      return parseInt(fee);
 }
 
- /*
-     * promptDialog({str, r, validateFn, actionFn});
-     *  This function will repeatedly prompt the user
-     *  until either a valid response is entered
-     *  or the prompt dialog is cancelled.
-     */
-function promptDialog(data, carStatus)
+function promptDialog(data)
 {
      var response = data.r;
-     while(true) { //prompt repeatedly until use provides a valid response or cancels
+     while(true) { //prompt loop til valid response or cancel
           var response = prompt(data.str, response);
           if (response === null) {
                break;
           } //user cancelled
           if (!data.validateFn || typeof data.validateFn !== 'function') {
-               return response; //assume OK if no validate function is specified
+               return response; //if no validate
           }
           if (data.validateFn(response)) {
-               if (data.actionFn && typeof data.actionFn == 'function') {//if an action is specified
-                    data.actionFn(response);//perform the action
+               if (data.actionFn && typeof data.actionFn == 'function') { //if an action is specified
+                    data.actionFn(response); //perform the action
                }
                return response;
           }
      }
-     return false;//return false if we broke out of the while loop
+     return false;
 }
